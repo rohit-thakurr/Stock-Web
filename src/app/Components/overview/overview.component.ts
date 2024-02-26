8cc98657a929159e2be7ed52abbf920640eb379c
Rohit Thakur <rprohitprajapati2000@gmail.com>
2024-02-27 04:07:26 +0530

    changes chart

--- src/app/Components/overview/overview.component.ts
+++ src/app/Components/overview/overview.component.ts
@@ -10,15 +10,23 @@ export class OverviewComponent implements OnInit{
 
   stock: any ;
   img : string = "";
+  btn: number = 1;
   constructor(private stockService: StocksService ){};
 
   ngOnInit(): void {
     this.stockService.getHistory().subscribe(async (data) => {
      console.log(data);
      this.stock = data;
-     this.img = this.stock.Chart30dPath;
-     
+     this.img = this.stock.ChartTodayPath;
+     console.log(this.stock.ChartTodayPath);
     });
+  }
+
+  timeLine(val : number){
+    this.btn = val;
+    if(val == 1) this.img = this.stock.ChartTodayPath;
+    else if(val == 2) this.img = this.stock.Chart30dPath;
+    else if(val == 3) this.img = this.stock.Chart365dPath;
   }
   
 }
